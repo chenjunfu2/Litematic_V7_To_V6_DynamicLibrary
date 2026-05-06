9dc2f39aca6306dc1fbdaf17c80ec484ca1e288d
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-07 01:52:28 +0800

    Simplify cmake-build.bat by removing variable assignments

--- cmake-build.bat
+++ cmake-build.bat
@@ -1,18 +1,4 @@
 @echo off
 
-set SLN_FILE=Litematic_V7_To_V6_DynamicLibrary.sln
-set CONFIG=Release
-set PLATFORM=x64
-
-set "MSBUILD_PATH="
-for /f "delims=" %%i in ('vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\Current\Bin\MSBuild.exe') do (
-    set "MSBUILD_PATH=%%i"
-)
-
-if not defined MSBUILD_PATH (
-    echo ERROR: MSBuild.exe not found.
-    exit /b 1
-)
-
-"%MSBUILD_PATH%" %SLN_FILE% /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /p:PlatformToolset=v142 /m
+msbuild Litematic_V7_To_V6_DynamicLibrary.sln /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v142 /m
 if %errorlevel% neq 0 exit /b %errorlevel%
