a58f94d634b48db83b8f99fecb562940cca20f01
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-06 16:04:32 +0800

    Update cmake-build.sh

--- cmake-build.sh
+++ cmake-build.sh
@@ -2,4 +2,12 @@
 set -e
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3 -DNDEBUG -g0" -S .
 cmake --build build
-strip --strip-all ./build/Litematic_V7_To_V6_DynamicLibrary/Litematic_V7_To_V6_DynamicLibrary.so
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    strip -Sx ./build/Litematic_V7_To_V6_DynamicLibrary/Litematic_V7_To_V6_DynamicLibrary.dylib
+elif [[ "$(uname)" == "Linux" ]]; then
+    strip --strip-all ./build/Litematic_V7_To_V6_DynamicLibrary/Litematic_V7_To_V6_DynamicLibrary.so
+else
+    echo "Unsupported OS: $(uname)"
+    exit 1
+fi
