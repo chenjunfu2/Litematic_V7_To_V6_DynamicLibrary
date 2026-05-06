d5c57ec0846a942a88a73b64f1c9e3da700ec3c0
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-07 02:20:21 +0800

    Add artifact creation for Linux build
    Create artifacts directory and copy the built shared library.

--- cmake-build-linux.sh
+++ cmake-build-linux.sh
@@ -5,3 +5,6 @@ cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3 -DNDEBU
 cmake --build build
 
 strip --strip-all ./build/Litematic_V7_To_V6_DynamicLibrary/Litematic_V7_To_V6_DynamicLibrary.so
+
+mkdir -p artifacts/linux-native-x64
+cp -f ./build/Litematic_V7_To_V6_DynamicLibrary/Litematic_V7_To_V6_DynamicLibrary.so artifacts/linux-native-x64/
