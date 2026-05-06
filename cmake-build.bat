bd8207694338f907ff1250c6ffddbadf3a95400a
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-06 15:33:27 +0800

    Update cmake-build.bat

--- cmake-build.bat
+++ cmake-build.bat
@@ -4,5 +4,7 @@ set SLN_FILE=Litematic_V7_To_V6_DynamicLibrary.sln
 set CONFIG=Release
 set PLATFORM=x64
 
-msbuild %SLN_FILE% /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /p:PlatformToolset=v142 /m
+for /f "delims=" %%i in ('vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\Current\Bin\MSBuild.exe') do set "MSBUILD_PATH=%%i"
+
+%MSBUILD_PATH% %SLN_FILE% /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /p:PlatformToolset=v142 /m
 if %errorlevel% neq 0 exit /b %errorlevel%
