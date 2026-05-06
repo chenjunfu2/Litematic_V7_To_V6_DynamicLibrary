3cdd1c19bfa1a10d883452c979c30e6d67bfc37b
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-07 02:40:03 +0800

    Fix directory creation and copy paths in build script

--- cmake-build-windows.bat
+++ cmake-build-windows.bat
@@ -3,5 +3,5 @@
 msbuild Litematic_V7_To_V6_DynamicLibrary.sln /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v142 /m
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-mkdir artifacts/windows-x64
-copy /Y ./x64/Release/Litematic_V7_To_V6_DynamicLibrary.dll artifacts/windows-x64/
+mkdir .\artifacts\windows-x64
+copy /Y .\x64\Release\Litematic_V7_To_V6_DynamicLibrary.dll .\artifacts\windows-x64\
