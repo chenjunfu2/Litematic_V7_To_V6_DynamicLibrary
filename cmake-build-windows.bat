0e0c1cb56a534364c36c6fefd384c3bae2938843
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-10 15:07:31 +0800

    fix

--- cmake-build-windows.bat
+++ cmake-build-windows.bat
@@ -14,7 +14,7 @@ if not defined MSBUILD (
 )
 echo MSBuild path "%MSBUILD%"
 
-for /f "usebackq delims=" %i in (`"!VSWHERE!" -latest -find **\devenv.com`) do "%i" Litematic_V7_To_V6_DynamicLibrary.sln /Upgrade
+for /f "usebackq delims=" %%i in (`"!VSWHERE!" -latest -find **\devenv.com`) do "%%i" Litematic_V7_To_V6_DynamicLibrary.sln /Upgrade
 del /Q UpgradeLog*.htm 2>nul
 
 "%MSBUILD%" Litematic_V7_To_V6_DynamicLibrary.sln /p:Configuration=Release /p:Platform=x64 /m
