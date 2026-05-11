3e8107790f33ce0768e0f19992d4dc75b47fae14
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-05-12 02:21:18 +0800

    fix win bash for

--- cmake-build-windows.bat
+++ cmake-build-windows.bat
@@ -7,14 +7,31 @@ if not exist "!VSWHERE!" (
     exit /b 1
 )
 
-for /f "usebackq delims=" %%i in (`"!VSWHERE!" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do set "MSBUILD=%%i"
+set "MSBUILD="
+for /f "usebackq delims=" %%i in (`"!VSWHERE!" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+    set "MSBUILD=%%i"
+    goto :MSBuildFound
+)
+:MSBuildFound
 if not defined MSBUILD (
     echo Error: No MSBuild found by vswhere.
     exit /b 1
 )
 echo MSBuild path "%MSBUILD%"
 
-for /f "usebackq delims=" %%i in (`"!VSWHERE!" -latest -find **\devenv.com`) do "%%i" Litematic_V7_To_V6_DynamicLibrary.sln /Upgrade
+set "DEVENV="
+for /f "usebackq delims=" %%i in (`"!VSWHERE!" -latest -find **\devenv.com`) do (
+    set "DEVENV=%%i"
+    goto :DevenvFound
+)
+:DevenvFound
+if not defined DEVENV (
+    echo Error: No devenv.com found by vswhere. Cannot upgrade solution.
+    exit /b 1
+)
+echo Upgrading solution with "%DEVENV%"
+
+"%DEVENV%" Litematic_V7_To_V6_DynamicLibrary.sln /Upgrade
 del /Q UpgradeLog*.htm 2>nul
 
 "%MSBUILD%" Litematic_V7_To_V6_DynamicLibrary.sln /p:Configuration=Release /p:Platform=x64 /m
