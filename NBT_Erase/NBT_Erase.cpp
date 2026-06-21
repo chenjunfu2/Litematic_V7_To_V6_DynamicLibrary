3ca10257a650b67d59d038c3f20cb6e1fab1a118
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-06-21 21:42:25 +0800

    添加nbt删除功能

---
+++ NBT_Erase/NBT_Erase.cpp
@@ -0,0 +1,48 @@
+﻿#include <nbt_cpp/NBT_All.hpp>
+
+
+
+
+struct EraseRequest
+{
+public:
+	enum EraseMode
+	{
+		REMOVE,
+		CLEAR,
+	};
+
+	struct SegPathInfo
+	{
+		bool bIsList;//列表特殊处理（下标处理）
+		NBT_Type::String strSegmentPath;
+	};
+
+	using PathInfo = std::vector<SegPathInfo>;
+
+	static PathInfo FullPathToPathInfo(const NBT_Type::String &strFullPath)
+	{
+
+
+
+
+	}
+
+public:
+	PathInfo vPath;
+	EraseMode enMode;
+};
+
+
+using RequestList = std::vector<EraseRequest>;
+
+
+
+
+
+
+
+
+
+
+
